0abe3340f999dfe9715870537203e07d0c5db5de
pohlinkzei <pohlinkzei@gmx-topmail.de>
2015-11-26 23:48:27 +0100

    add starter

--- processcontrol.sh
+++ processcontrol.sh
@@ -3,12 +3,13 @@
 while true;
 do
 
-if pgrep powercontrol >/dev/null
-	then
-		sleep 1
-	else
-		sudo ~/powercontrol/powercontrol
-		sleep 5
+if pgrep "powercontrol" >/dev/null
+then
+	sleep 1
+else
+	echo "Restarting PowerControl"
+	sudo ./powercontrol
+	sleep 5
 fi
-
+sleep 1
 done
